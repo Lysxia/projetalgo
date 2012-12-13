7dbc3866c9b2e8214195586a03a5de8920348b2a
Syrak <li-yao.xia@yahoo.fr>
2012-12-13 16:43:02 +0100

    Test strassen OK

--- test_strassen.sh
+++ test_strassen.sh
@@ -7,22 +7,22 @@ cat /proc/cpuinfo | grep -m 1 "model name" | sed 's/model name\t: //'
 echo -n "Revision: " 
 git rev-parse HEAD | cut -c1-10
 
-for i in {1..1000..1} # Start - stop - increment
+for i in {100..1000..1} # Start - stop - increment
 do 
-    echo $i
+    echo -n $i","
     ./rand2 $i $i $i | ./strassen
 done
 
 
 for i in {1000..2000..100} # Start - stop - increment
 do 
-    echo $i
+    echo $i","
     ./rand2 $i $i $i | ./strassen
 done
 
 
 for i in {2000..10000..1000} # Start - stop - increment
 do 
-    echo $i
+    echo $i","
     ./rand2 $i $i $i | ./strassen
 done
