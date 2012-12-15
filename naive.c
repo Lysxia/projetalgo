31f27ee05a651d2770e9d5e4b5c65e759b303f84
A <blob@blob.com>
2012-12-15 19:12:46 +0100

    Correction de "naive"

--- naive.c
+++ naive.c
@@ -2,19 +2,18 @@
 #include <stdio.h>
 
 #include "naive.h"
-#include "types.h"
 #include "naive_mult.h"
 #include "matrixio.h"
 
 
-mat_t naive_product (mat_t* matrices, int * sizes, int term_count)
+int* naive_product (int** matrices, int * sizes, int term_count)
 {
 
   return NULL; // TODO
 }
 
 
-int main (int argc, char ** argc)
+int main (int argc, char ** argv)
 {
 
   return 0;
